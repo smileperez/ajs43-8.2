1cc8fe31c364f6424a21ffc411d6d958e695d888
smileperez <aa.efimenko@yandex.ru>
2023-01-19 23:53:29 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './js/Team';
+import './js/Character';
+import './js/Bowman';
+import './js/Daemon';
+import './js/Magician';
+import './js/Swordsman';
+import './js/Undead';
+import './js/Zombie';
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+// const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  output: {
+    // path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+  ],
+};
